8e0d254e769e01f2a460437e4f12a9521c22923c
arjit39 <arjit39@my.yorku.ca>
2024-03-13 16:28:59 -0400

    add more graph ql packages to resolve problem

--- cpm-tool/src/App.js
+++ cpm-tool/src/App.js
@@ -3,13 +3,15 @@ import { Amplify } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import config from "./amplifyconfiguration.json";
-import ShowBlogs from "./components/ShowBlogs";
+import { ProjectCreateForm } from "./ui-components";
+
 Amplify.configure(config);
 
 function App({ signOut, user }) {
   return (
     <>
       <h1>Hello {user.username}</h1>
+      <ProjectCreateForm />
       <button onClick={signOut}>Sign out</button>
     </>
   );
